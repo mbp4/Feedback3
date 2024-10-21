a6baafd5dd497e17a7d786cfed2f3ffc4390431d
mbp4 <mblanpon@myuax.com>
2024-10-21 08:59:02 +0200

    ajustes para cerrar sesion

--- app/src/main/java/com/example/feedback3/AjustesActivity.kt
+++ app/src/main/java/com/example/feedback3/AjustesActivity.kt
@@ -1,11 +1,33 @@
 package com.example.feedback3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AjustesActivity:AppCompatActivity() {
+    private lateinit var btnMain: Button
+    private lateinit var btnCerrar: Button
+    private lateinit var textoInfo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
+
+        btnMain = findViewById(R.id.btnCerrarSesion)
+        btnCerrar = findViewById(R.id.btnMain)
+        textoInfo = findViewById(R.id.textMailInfo)
+
+        btnMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCerrar.setOnClickListener {
+            Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
